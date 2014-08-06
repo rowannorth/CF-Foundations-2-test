f750c2fdb821c8e7daa557e575c7dd0f9d91a050
Rowan <rowanowlnorth@gmail.com>
2014-08-06 11:50:17 -0700

    Initial Commit

---
+++ CF iOS F2 homework 1Tests/CF_iOS_F2_homework_1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  CF_iOS_F2_homework_1Tests.swift
+//  CF iOS F2 homework 1Tests
+//
+//  Created by Rowan North on 8/6/14.
+//  Copyright (c) 2014 Rowan North. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CF_iOS_F2_homework_1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
