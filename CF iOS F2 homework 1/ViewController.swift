c3d25acc358fc4fe932c7652f8e2b398499080e3
Rowan <rowanowlnorth@gmail.com>
2014-08-06 18:59:05 -0700

    Adds button, switch button, and slider that all send messages to console when triggered.

--- CF iOS F2 homework 1/ViewController.swift
+++ CF iOS F2 homework 1/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+     var buttonAnnouncement = "Uh oh, you pressed my button!"
+     var switchMessage = "Yep, you toggled my switch."
+     var sliderMessage = "That slider sure does work."
+    
+    @IBAction func buttonPress(sender: AnyObject) {
+       
+        println(buttonAnnouncement)
+    }
+    @IBOutlet weak var mySwitch: UISwitch!
+    
+    @IBAction func sendMessage(sender: UISwitch) {
+        println(switchMessage)
+    }
+    @IBOutlet weak var mySlider: UISlider!
+        var name = "Rowan"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.mySlider.value = 0.0
     }
-
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        self.mySlider.setValue(0.0, animated: true)
+    }
+    @IBAction func sliderDidSlide(sender: UISlider, forEvent event: UIEvent) {
+        if self.mySlider.value > 0.5 {
+        println(sliderMessage)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
